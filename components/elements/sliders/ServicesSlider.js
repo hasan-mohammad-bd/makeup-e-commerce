8f375d8210c39bff53a6df4d10744b67d44f7eb9
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-10-19 18:58:03 +0600

    service slider added

---
+++ components/elements/sliders/ServicesSlider.js
@@ -0,0 +1,85 @@
+"use client";
+
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const ServicesSlider = () => {
+	return (
+		<>
+			<Swiper
+				// modules={[Navigation]}
+				slidesPerView={4}
+				spaceBetween={20}
+				loop={false}
+				breakpoints={{
+					0: {
+						slidesPerView: 2,
+						spaceBetween: "140px",
+					},
+					768: {
+						slidesPerView: 4,
+					},
+				}}
+			>
+				<SwiperSlide>
+					<div className="col-span-3 flex items-center gap-4 w-[236px]">
+						<Image
+							src="/assets/images/icons/parcel.png"
+							alt="Replacement"
+							width={48}
+							height={48}
+						/>
+						<p className="text-base/[22px] font-semibold font-title">
+							<span className="text-primary">৭ দিনের মধ্যে</span> বিনামূল্যে
+							পরিবর্তনযোগ্য
+						</p>
+					</div>{" "}
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className="single-feature col-span-3 flex items-center gap-4 w-[236px]">
+						<Image
+							src="/assets/images/icons/delivery.png"
+							alt="Replacement"
+							width={48}
+							height={48}
+						/>
+						<p className="text-base/[22px] font-semibold font-title">
+							ঢাকার মধ্যে{" "}
+							<span className="text-primary">ফ্রি হোম ডেলিভারি</span>
+						</p>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className="single-feature col-span-3 flex items-center gap-4 w-[236px]">
+						<Image
+							src="/assets/images/icons/pay.png"
+							alt="Replacement"
+							width={48}
+							height={48}
+						/>
+						<p className="text-base/[22px] font-semibold font-title">
+							<span className="text-primary">নিরাপদে পেমেন্ট</span> করার সহজ
+							মাধ্যম
+						</p>
+					</div>
+				</SwiperSlide>
+				<SwiperSlide>
+					<div className="single-feature col-span-3 flex items-center gap-4 w-[236px]">
+						<Image
+							src="/assets/images/icons/support.png"
+							alt="Replacement"
+							width={48}
+							height={48}
+						/>
+						<p className="text-base/[22px] font-semibold font-title">
+							<span className="text-primary">সর্বাক্ষনিক</span> ও দ্রুত গ্রাহক
+							সেবা{" "}
+						</p>
+					</div>
+				</SwiperSlide>
+			</Swiper>
+		</>
+	);
+};
+
+export default ServicesSlider;
