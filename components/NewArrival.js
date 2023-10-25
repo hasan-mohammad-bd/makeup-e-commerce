6b486b247aa5f196ec9707c9213de6b868eadf38
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-10-25 14:44:55 +0600

    new arrival products adjusted

--- components/NewArrival.js
+++ components/NewArrival.js
@@ -4,24 +4,41 @@ import { fetchData } from "@/utils/fetchData";
 import { HiArrowLongRight } from "react-icons/hi2";
 
 const NewArrival = async () => {
-	const data = await fetchData({ api: "product-latest" });
-	const newProducts = data?.data || [];
+	// const data = await fetchData({ api: "product-latest" });
+	// const newProducts = data?.data || [];
+
+	const [newProductRes, translationRes] = await Promise.allSettled([
+		fetchData({ api: `product-latest` }),
+		fetchData({ api: `translations` }),
+	]);
+
+	const newProducts =
+		newProductRes.status === "fulfilled" ? newProductRes.value?.data || [] : [];
+	const translations =
+		translationRes.status === "fulfilled"
+			? translationRes.value?.data || {}
+			: {};
 
 	return (
 		<>
 			<div className="container">
 				<div className="sec-heading w-full flex justify-between items-center lg:border-b border-slate-200 pb-3">
-					<h2 className="sec-title">শপে নতুন এসেছে</h2>
-					<Link href="/products" className="all-btn !hidden lg:!block">
-						সবগুলো দেখুন <HiArrowLongRight size={24} />{" "}
+					<h2 className="sec-title capitalize">
+						{translations["new-in-shop"]}
+					</h2>
+					<Link
+						href="/products"
+						className="all-btn !hidden lg:!block capitalize"
+					>
+						{translations["see-all"]} <HiArrowLongRight size={24} />{" "}
 					</Link>
 				</div>
 
 				<div className="new-slider mt-4 lg:mt-6  relative">
 					<NewArrivalSlider newProducts={newProducts} />
 				</div>
-				<Link href="/products" className="border-btn mt-3 lg:hidden">
-					সবগুলো দেখুন <HiArrowLongRight size={24} />{" "}
+				<Link href="/products" className="border-btn mt-3 lg:hidden capitalize">
+					{translations["see-all"]} <HiArrowLongRight size={24} />{" "}
 				</Link>
 			</div>
 		</>
