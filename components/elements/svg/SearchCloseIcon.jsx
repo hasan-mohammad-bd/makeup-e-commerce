fb8e9649de12bdafd9ad1c33e969adf269b7635b
Hasan Mohammad <89174358+hasan-mohammad-bd@users.noreply.github.com>
2023-12-24 01:37:33 -0800

    Added brand search bar (#184)
    * adding brand search bar
    
    * UI fixed
    
    * adding button to see less and scroll bar
    
    * ui fixed

---
+++ components/elements/svg/SearchCloseIcon.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+const SearchCloseIcon = (props) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={18}
+		height={28}
+		fill="none"
+		{...props}
+	>
+		<path
+			stroke="#0F172A"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			strokeWidth={1.5}
+			d="M6 18L18 6M6 6L18 18"
+		/>
+	</svg>
+);
+export default SearchCloseIcon;
--- components/filters/BrandFilter.js
+++ components/filters/BrandFilter.js
@@ -1,74 +1,159 @@
 "use client";
 
-import { useState } from "react";
-// import { FiPlus } from "react-icons/fi";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import useSelectURLQuery from "@/hooks/useSelectURLQuery";
+import { FiSearch } from "react-icons/fi";
+import SearchCloseIcon from "../elements/svg/SearchCloseIcon";
+import { GoPlus } from "react-icons/go";
+import { FiMinus } from "react-icons/fi";
 
 const BrandFilter = ({ filteredBrands, selectedBrandIds }) => {
-	const [showAll, setShowAll] = useState(false);
-	const { translations } = useSelector((state) => state.common);
-	const { handleSelectChange } = useSelectURLQuery();
-	let brands = filteredBrands || [];
+  const [isSearch, setIsSearch] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showAll, setShowAll] = useState(false);
+  const { translations } = useSelector((state) => state.common);
+  const { handleSelectChange } = useSelectURLQuery();
+  const searchRef = useRef(null);
 
-	if (!showAll && brands?.length > 6) {
-		brands = brands?.slice(0, 6);
-	}
+  let searchFilteredBrands = filteredBrands || [];
 
-	//Handling Brand select
-	const handleChange = (e) => {
-		if (e.target.checked) {
-			const ids = [...selectedBrandIds, e.target.value];
-			handleSelectChange("brand_ids", ids.join(","));
-		} else {
-			handleSelectChange(
-				"brand_ids",
-				selectedBrandIds.filter((id) => id !== `${e.target.value}`).join(",")
-			);
-		}
-	};
+  // console.log(searchFilteredBrands)
 
-	return (
-		<>
-			<div className="pr-5">
-				<h6 className="text-sm font-bold text-slate-900 border-b border-slate-200 pb-3">
-					{translations["brands"] || "ব্র্যান্ড সমূহ"}
-				</h6>
-				<div className="category-filter">
-					{brands?.map((brand) => (
-						<div className="input-grp mt-3" key={brand.id}>
-							<label
-								className="flex items-center gap-2 text-base text-slate-700 cursor-pointer"
-								htmlFor={`brd-${brand.id}`}
-							>
-								<input
-									type="checkbox"
-									id={`brd-${brand.id}`}
-									checked={selectedBrandIds.includes(`${brand.id}`)}
-									name={brand.brand_name}
-									value={brand.id}
-									onChange={handleChange}
-								/>
-								{brand.brand_name}
-							</label>
-						</div>
-					))}
-					{!showAll && filteredBrands?.length > 6 ? (
-						<div className="text-center mt-3">
-							<span
-								onClick={() => setShowAll(true)}
-								className="inline-flex items-center text-primary cursor-pointer"
-							>
-								{/* <FiPlus size={20} className="mr-2" /> */}
-								{translations["see-all"] || "আরও"}
-								{/* {filteredBrands?.length - 6} */}
-							</span>
-						</div>
-					) : null}
-				</div>
-			</div>
-		</>
-	);
+  if (searchTerm) {
+    searchFilteredBrands = filteredBrands.filter((brand) =>
+      brand.brand_name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  let brands = searchFilteredBrands || [];
+
+  if (!showAll && brands?.length > 6) {
+    brands = brands?.slice(0, 6);
+  }
+
+  //Handling Brand select
+  const handleChange = (e) => {
+    if (e.target.checked) {
+      const ids = [...selectedBrandIds, e.target.value];
+      handleSelectChange("brand_ids", ids.join(","));
+    } else {
+      handleSelectChange(
+        "brand_ids",
+        selectedBrandIds.filter((id) => id !== `${e.target.value}`).join(",")
+      );
+    }
+  };
+
+  const handleSearchClose = () => {
+    setIsSearch(false);
+    setSearchTerm("");
+  };
+
+  useEffect(() => {
+    if (isSearch) {
+      searchRef.current.focus();
+    }
+  }, [isSearch]);
+
+  return (
+    <>
+      <div className="pr-5">
+        <div className={`w-full qna-search pt border-b border-slate-200 pb-2 `}>
+          {!isSearch ? (
+            <div className="flex justify-between items-center pt-2 pr-2">
+              <h6 className="text-sm font-bold text-slate-900">
+                {translations["brands"] || "ব্র্যান্ড সমূহ"}
+              </h6>
+              <button
+                className="w-[30px] h-[30px] bg-white rounded-full border-2 border-[#E2E8F0] flex justify-center items-center  mt-[-4px]"
+                onClick={() => setIsSearch(true)}
+              >
+                <FiSearch size={18} />
+              </button>
+            </div>
+          ) : (
+            <div
+              className={`w-full  question-search flex items-center relative mt-3 lg:mt-0`}
+            >
+              <div className="relative w-full">
+                <input
+                  ref={searchRef}
+                  type="text"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  placeholder={
+                    translations["search-for-brands"] || "ব্রান্ড সার্চ করুন"
+                  }
+                  className="h-[2.1rem] w-full border border-[#E2E8F0] bg-slate-100 lg:bg-white focus:bg-white focus:border-primary focus:outline-none rounded-full"
+                />
+              </div>
+              <button
+                onClick={handleSearchClose}
+                className=" ml-6  absolute  right-[11px] top-[6px]"
+              >
+                <SearchCloseIcon className="" />
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className={`category-filter mt-3`}>
+          <div
+            className={` ${
+              showAll ? "h-[300px] overflow-auto scrollbar-hide" : ""
+            }   `}
+          >
+            {brands?.map((brand) => (
+              <div className={`input-grp mb-3`} key={brand.id}>
+                <label
+                  className="flex items-center gap-2 text-base text-slate-700 cursor-pointer"
+                  htmlFor={`brd-${brand.id}`}
+                >
+                  <input
+                    type="checkbox"
+                    id={`brd-${brand.id}`}
+                    checked={selectedBrandIds.includes(`${brand.id}`)}
+                    name={brand.brand_name}
+                    value={brand.id}
+                    onChange={handleChange}
+                  />
+                  {brand.brand_name}
+                </label>
+              </div>
+            ))}
+          </div>
+
+          {!showAll && searchFilteredBrands?.length > 6 ? (
+            <div className="text-center mt-3">
+              <span
+                onClick={() => setShowAll(true)}
+                className="inline-flex items-center text-primary cursor-pointer"
+              >
+                <span className="mr-1">
+                  <GoPlus />
+                </span>
+                {translations["see-all"] || "আরও"}
+              </span>
+            </div>
+          ) : null}
+          {showAll ? (
+            <div className="text-center mt-3">
+              <span
+                onClick={() => setShowAll(false)}
+                className="inline-flex items-center text-primary cursor-pointer"
+              >
+                <span className="mr-1">
+                  <FiMinus />
+                </span>
+                {translations["see-less"] || "কমান"}
+              </span>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BrandFilter;
